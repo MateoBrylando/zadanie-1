e76e87a2bcc5640abc11c4fc4d75a16968e81e0a
MateoBrylando <161622218+MateoBrylando@users.noreply.github.com>
2024-03-13 16:01:19 +0000

    Zadanie 1 czesc 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,12 @@ public class Main {
   public static void main(String[] args) {
     String tekst = "Hello World";
     for (int i = 0; i < tekst.length(); i++) {
+        for(int j = 0; j < i ; j++){
+          System.out.print(" ");
+        }
         System.out.println(tekst.charAt(i));
+        
+      
     }
   }
 }
