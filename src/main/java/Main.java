8e4a16fc128168230716bdad7d2cda0ed8fa0dfc
MateoBrylando <161622218+MateoBrylando@users.noreply.github.com>
2024-03-13 16:25:37 +0000

    zdanko 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,27 @@
+
+
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-
+import java.util.Random;
 public class Main {
   public static void main(String[] args) {
+    int suma = 0;
     
-        for (int i= 0; i <=100; i++){
-          if (i % 4 ==0){
+   Random rand = new Random(); 
+      for(int i = 0; i<5; i++){
 
-            System.out.println(+ i);
-            
-          }
-  
+         int randomNum = rand.nextInt(101);
+         System.out.println(randomNum);
+        int iloczyn = randomNum * randomNum;
+        System.out.println("Iloraz tej liczby to : " + iloczyn);
+        suma += iloczyn;
+
+        
+      }
+      System.out.println("Suma wszystkich ilorazów to : " + suma);
           
         }
       
     }
-  }
+  
