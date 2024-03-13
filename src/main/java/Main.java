ba6284a611492d67c7ed58ac34fccd06b1479b27
MateoBrylando <161622218+MateoBrylando@users.noreply.github.com>
2024-03-13 15:37:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    String tekst = "Hello World";
+    for (int i = 0; i < tekst.length(); i++) {
+        System.out.println(tekst.charAt(i));
+    }
+  }
+}
